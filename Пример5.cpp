9ef1d8020fbe153d0567d277dbca43c8468f7f52
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-05-06 16:10:35 +0800

    Пример 5

---
+++ Пример5.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+using namespace std;
+
+void test1() {
+	cout << "Executing test 1..." << endl;
+}
+
+void test2() {
+	cout << "Executing test 2..." << endl;
+}
+
+void test3() {
+	cout << "Executing test 3..." << endl;
+}
+
+void(*tests[])(void) {
+	test1, test2, test3
+};
+
+int main()
+{
+	for (size_t i = 0; i < sizeof(tests) / sizeof(tests[0]); i++)
+	{
+		tests[i]();
+	}
+}
+
