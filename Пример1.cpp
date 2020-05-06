58d19c89a291b9365511b631f10b408f6b50c76c
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-05-06 16:08:58 +0800

    Пример 1

---
+++ Пример1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+using namespace std;
+
+double area(double R) {
+	return M_PI * R * R;
+}
+
+int main()
+{
+	cout << "Enter radius of circle" << endl;
+	double radius = 0;
+	cin >> radius;
+	double(*func)(double);
+	func = area;
+	cout << "Area of this citcle is: " << func(radius) << endl;
+}
