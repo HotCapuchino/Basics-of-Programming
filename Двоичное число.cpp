5c8cbdb89e0cc49ae736f032e6b0831f8cce0ba4
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-05-27 23:39:36 +0800

    Перевод в двоичую систему

---
+++ Двоичное число.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+void toBinary(int num) {
+	if (num <= 0) {
+		return;
+	}
+	else {
+		toBinary(num / 2);
+		cout << num % 2;
+	}
+}
+
+int main()
+{
+    cout << "Enter your number:\n";
+	int num;
+	cin >> num;
+	toBinary(num);
+}
+
