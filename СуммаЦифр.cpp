c8bbe7da1c93feb9c39a17b5fd2f35d678ddd1b8
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-05-27 23:37:05 +0800

    Сумма

---
+++ СуммаЦифр.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include <iostream>
+
+
+int summ(int m, int buf) {
+	buf += m % 10;
+	if (m < 10) {
+		return buf;
+	}
+	else {
+		summ(m / 10, buf);
+	}
+
+}
+
+using namespace std;
+int main()
+{
+
+    cout << "Enter number: \n";
+	int m;
+	cin >> m;
+	if (m < 10) {
+		cout << m;
+	}
+	else {
+		cout << summ(m, 0);
+	}
+}
+
