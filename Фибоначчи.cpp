d5b6797332fb702d71c15321d7f475609dff0990
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-05-27 23:39:03 +0800

    Фибоначчи

---
+++ Фибоначчи.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include <iostream>
+
+int findFibonacci(int a) {
+	if (a == 0) {
+		return 0;
+	}
+	else if (a == 1 || a == 2)
+	{
+		return 1;
+	}
+	else {
+		return findFibonacci(a - 1) + findFibonacci(a - 2);
+	}
+}
+
+using namespace std;
+int main()
+{
+    cout << "Enter the position of fibonacci number:\n";
+	int n;
+	cin >> n;
+	cout << findFibonacci(n - 1);
+}
+
