ce5e151130a70f6900c109a4927e8ef47f549667
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-05-27 23:37:39 +0800

    Быстрая степень

---
+++ QuickPower.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include <iostream>
+
+float quickPower(float num, int power) {
+	if (power == 0) {
+		return 1;
+	}
+	else if (power % 2 != 0)
+	{
+		return (num * quickPower(num, power - 1));
+	}
+	else {
+		return quickPower(num * num, power / 2);
+	}
+}
+
+using namespace std;
+int main()
+{
+   cout << "Enter number:\n";
+   float x;
+   cin >> x;
+   cout << "Enter the power:\n";
+   int n;
+   cin >> n;
+   if (n <= 0 && x == 0) {
+	   cout << "Wrong data";
+	   return -1;
+   }
+   cout << quickPower(x, n);
+}
