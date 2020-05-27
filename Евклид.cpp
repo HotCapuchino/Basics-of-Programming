caad9d836724d5beff2cf953f004288dcf84a086
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-05-27 23:38:22 +0800

    Алгоритм Евклида

---
+++ Евклид.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include <iostream>
+
+int findDivider(int a, int b) {
+	if (a == 0 || b == 0) {
+		return a + b;
+	}
+	if (a > b)
+	{
+		findDivider(a % b, b);
+	}
+	else {
+		findDivider(a, b % a);
+	}
+}
+
+using namespace std;
+int main()
+{
+    cout << "Enter first number:\n";
+	int a;
+	cin >> a;
+	cout << "Enter second number:\n";
+	int b;
+	cin >> b;
+	cout << "The biggest general divider is: " << findDivider(a, b);
+}
+
