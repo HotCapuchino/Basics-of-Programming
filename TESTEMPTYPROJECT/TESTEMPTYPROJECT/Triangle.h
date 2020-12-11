642b7dae2a2cbd4709cf5e7164dd7babf640d99c
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-12-11 20:06:30 +0800

    Update Triangle.h

--- TESTEMPTYPROJECT/TESTEMPTYPROJECT/Triangle.h
+++ TESTEMPTYPROJECT/TESTEMPTYPROJECT/Triangle.h
@@ -10,8 +10,8 @@ public:
 	Triangle(Point* arr);
 	double getPerimetr();
 	double getSquare();
+	std::string printClass();
 	~Triangle(); 
 	friend std::ostream& operator<<(std::ostream& out, Triangle& triangle);
 	friend std::fstream& operator>>(std::fstream& in, Triangle& triangle);
 };
-
