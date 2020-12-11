c088107b0f4fb7e9fb9c68185cd5c51099a94c90
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-12-11 20:05:50 +0800

    Update Source.cpp

--- TESTEMPTYPROJECT/TESTEMPTYPROJECT/Source.cpp
+++ TESTEMPTYPROJECT/TESTEMPTYPROJECT/Source.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "Figure.h"
 #include "Triangle.h"
 #include "Point.h"
 #include "Circle.h"
 #include "Rectangle.h"
-#include <fstream>
-#include <string>
 #include "Poligon.h"
 
 using namespace std;
@@ -17,11 +19,11 @@ int main(void) {
 	triangle_points[1] = second;
 	triangle_points[2] = third;
 	Triangle triangle = Triangle(triangle_points);
-	cout << triangle.getPerimetr() << endl;
-	cout << triangle.getSquare() << endl;
+	/*cout << triangle.getPerimetr() << endl;
+	cout << triangle.getSquare() << endl;*/
 	Circle circle = Circle(5);
-	cout << circle.getPerimetr() << endl;
-	cout << circle.getSquare() << endl;
+	/*cout << circle.getPerimetr() << endl;
+	cout << circle.getSquare() << endl;*/
 	Point first_rect = Point(2, 0);
 	Point second_rect = Point(2, 4);
 	Point third_rect = Point(0, 4);
@@ -30,8 +32,8 @@ int main(void) {
 	rect_points[1] = second_rect;
 	rect_points[2] = third_rect;
 	Rectangle rect = Rectangle(rect_points);
-	cout << rect.getPerimetr() << endl;
-	cout << rect.getSquare();
+	/*cout << rect.getPerimetr() << endl;
+	cout << rect.getSquare();*/
 	ifstream in; 
 	in.open("res/polygon.txt");
 	if (!in.is_open()) {
@@ -48,9 +50,20 @@ int main(void) {
 		polygon_angles[i] = Point(x, y);
 	}
 	Poligon polygon = Poligon(polygon_angles, length);
-	cout << polygon.getPerimetr() << endl;
+	/*cout << polygon.getPerimetr() << endl;
 	cout << polygon.getSquare() << endl;
-	cout << polygon;
+	cout << polygon;*/
+	vector<Figure*> figures_array;
+	figures_array.push_back(&triangle);
+	figures_array.push_back(&circle);
+	figures_array.push_back(&rect);
+	figures_array.push_back(&polygon);
+	for (int i = 0; i < figures_array.size(); i++)
+	{
+		cout << "Figure # " << i + 1 << " " << figures_array.at(i)->printClass() << ":" << endl;
+		cout << "perimetr: " << figures_array.at(i)->getPerimetr() << endl;
+		cout << "square: " << figures_array.at(i)->getSquare() << endl;
+	}
 	system("pause");
 	return 1;
 }
