cdb9c4570180cca21c4ff7f6a17fe8c5d5a7d002
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-12-11 20:03:15 +0800

    Update Figure.h

--- TESTEMPTYPROJECT/TESTEMPTYPROJECT/Figure.h
+++ TESTEMPTYPROJECT/TESTEMPTYPROJECT/Figure.h
@@ -8,7 +8,7 @@ public:
 	virtual ~Figure();
 	virtual double getSquare() = 0;
 	virtual double getPerimetr() = 0;
+	virtual std::string printClass() = 0;
 	friend std::istream& operator>>(std::istream& is, Figure& figure);
 	friend std::ostream& operator<<(std::ostream& out, Figure& figure);
 };
-
