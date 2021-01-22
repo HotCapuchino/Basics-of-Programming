463a5a8cf4406eb4c74ff73fd39bb3cd2aaa5444
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2021-01-22 15:10:09 +0800

    refactoring

--- QuadriplePrecision/QuadriplePrecision/main.cpp
+++ QuadriplePrecision/QuadriplePrecision/main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main(void) {
 	/*cout << "Hello World!\n";*/
-	QuadriplePrecision num1 = QuadriplePrecision("12.25");
+	QuadriplePrecision num1 = QuadriplePrecision("12.25", 112, 16);
 	cout << "num1:\n";
 	num1.printScientificNotation();
-	QuadriplePrecision num2 = QuadriplePrecision("4.25");
+	QuadriplePrecision num2 = QuadriplePrecision("4.25", 112, 16);
 	cout << "\nnum2:\n";
 	num2.printScientificNotation();
 	/*num1.printBinaryRepresentation();
